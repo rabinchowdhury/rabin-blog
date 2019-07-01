6321a7ea3d6433d25a17b6286f88de5416979ce8
rabinchowdhury <rabinfed@gmail.com>
2019-07-01 14:49:38 -0400

    added cover-page

---
+++ src/components/coverpage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CoverPage extends Component {
+    render() {
+        return (
+            <div className="cover-text">
+                <div className="cover-page">
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CoverPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,31 @@ import React from "react";
 import { Link } from "gatsby";
 
 import Layout from "../components/layout";
-// import Image from "../components/image";
 import SEO from "../components/seo";
 // import "./index.css";
+
+import CoverPage from "../components/coverpage"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <div className="home">
-      <h1>Hello There</h1>
-      <p>Welcome to my awesome blog</p>
-      <div>
+    <div>
+      <CoverPage />
+      <div className="home">
+        <h1>Hello There</h1>
+        <p>Welcome to my awesome blog</p>
         <div>
-          {/* <Image /> */}
+          <div
+            style={{
+            }}
+          >
+          </div>
         </div>
+        <Link to="/blogposts/">View all posts</Link>
       </div>
-      <Link to="/blogposts/">View all posts</Link>
     </div>
   </Layout>
 );
 
 export default IndexPage;
+
